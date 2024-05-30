ab15ace0806e3f6a1293d9fe315f587216978dcc
Jarryd Deane <Jarryd.deane@gmail.com>
2024-05-30 16:36:18 +0200

    Add multiple get endpoints so that we can add Redis Caching

--- CachingWithRedis/Controllers/CustomerController.cs
+++ CachingWithRedis/Controllers/CustomerController.cs
@@ -20,4 +20,64 @@ public class CustomerController : ControllerBase
     {
         return _dbContext.Customers.ToList();
     }
+
+    [HttpGet("{id}")]
+    public ActionResult<Customer> GetCustomerById(int id)
+    {
+        var customer = _dbContext.Customers.Find(id);
+
+        if (customer is null)
+        {
+            return NotFound();
+        }
+
+        return customer;
+    }
+
+    [HttpPost]
+    public ActionResult<Customer> CreateCustomer([FromBody] Customer customer)
+    {
+        _dbContext.Customers.Add(customer);
+        _dbContext.SaveChanges();
+
+        return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateCustomer(int id, [FromBody] Customer updatedCustomer)
+    {
+        var customer = _dbContext.Customers.Find(id);
+
+        if (customer is null)
+        {
+            return NotFound();
+        }
+
+        customer.PhoneNumber = updatedCustomer.PhoneNumber;
+        customer.DateOfBirth = updatedCustomer.DateOfBirth;
+        customer.Address = updatedCustomer.Address;
+        customer.Email = updatedCustomer.Email;
+        customer.Firstname = updatedCustomer.Firstname;
+        customer.Lastname = updatedCustomer.Lastname;
+
+        _dbContext.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteCustomer(int id)
+    {
+        var customer = _dbContext.Customers.Find(id);
+        
+        if (customer is null)
+        {
+            return NotFound();
+        }
+
+        _dbContext.Customers.Remove(customer);
+        _dbContext.SaveChanges();
+
+        return NoContent();
+    }
 }
